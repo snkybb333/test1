1e6c3f9b641c21b8c4efbb788b47fbc755dde33d
rdh <rdh_319@qq.com>
2023-08-04 11:18:51 +0800

    no message

---
+++ Angry Birds/test1/Assets/scripts/bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+	private bool isclick = false;
+
+	private void OnMouseDown() { //按下
+		isclick =true;
+	}
+
+	private void OnMouseUp() {  //抬起
+		isclick =false;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		if (isclick) {
+			
+		}
+	}
+}
